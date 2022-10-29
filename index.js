d912084ec654fa85e4c9fb11df763a1297335b70
Jarno Varis <jarno.varis@gmail.com>
2022-10-29 13:03:58 +0300

    moved the logic of the blogs-routing into controller.. seems to still work

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
 //received this in an email
 
-const http = require('http')
+//const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controller/blogs')
 const mongoose = require('mongoose')
-const Blog = require('./models/blogs')
-
 const logger = require('./utils/logger')
 
 const config = require('./utils/config')
 
 mongoose.connect(config.MONGODB_URI)
 
+//const server = http.createServer(app)
+
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
