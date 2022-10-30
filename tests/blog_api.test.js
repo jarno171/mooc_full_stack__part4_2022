2f16276d08ff7aca9cd9433651b37a94b06815e1
Jarno Varis <jarno.varis@gmail.com>
2022-10-30 13:06:01 +0200

    making the backend tests, working version

---
+++ tests/blog_api.test.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5
+  },
+  {
+    title: 'Of being a Leevi',
+    author: 'Leevi L. Leevinen',
+    url: 'www.leevi.com',
+    likes: 5000
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  }, 100000)
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+test('a specific blog is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  const contents = response.body.map(r => r.title)
+  expect(contents).toContain(
+    'Go To Statement Considered Harmful'
+  )
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
