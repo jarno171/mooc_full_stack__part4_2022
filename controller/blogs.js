c3dd87c5de8e00588a8ba182ebce68758ae7f7a2
Jarno Varis <jarno.varis@gmail.com>
2022-11-03 20:26:22 +0200

    added token extractor as middleware

--- controller/blogs.js
+++ controller/blogs.js
@@ -27,19 +27,9 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 })
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogsRouter.post('/', async (request, response, next) => {
 
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
